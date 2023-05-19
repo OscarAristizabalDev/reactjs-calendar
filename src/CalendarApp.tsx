6f39dc281d9e1041017998923686a10ea643409b
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-05-19 15:36:24 -0500

    Se crea Slice Store para el manejo de todo lo que tenga que ver con UI

--- src/CalendarApp.tsx
+++ src/CalendarApp.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter } from "react-router-dom"
 import { AppRouter } from "./router"
+import { Provider } from "react-redux"
+import { store } from "./store"
 
 export const CalendarApp = () => {
     return (
-        // BrowserRouter es requerido para el manejo de rutas
-        <BrowserRouter> 
-            <AppRouter />
-        </BrowserRouter>
+        // Provee el store a toso los componentes hijos
+        <Provider store={store}>
+            // BrowserRouter es requerido para el manejo de rutas
+            <BrowserRouter>
+                <AppRouter />
+            </BrowserRouter>
+        </Provider>
     )
 }
